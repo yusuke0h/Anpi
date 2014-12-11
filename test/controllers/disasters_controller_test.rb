65915763687b3545838827116677ea8465217b95
Yusuke Hosoi <yusuke0.hosoi@gmail.com>
2014-12-11 17:03:15 +0900

    scaffoldのやり直し

--- test/controllers/disasters_controller_test.rb
+++ test/controllers/disasters_controller_test.rb
@@ -1,9 +1,14 @@
 require 'test_helper'
 
 class DisastersControllerTest < ActionController::TestCase
+  setup do
+    @disaster = disasters(:one)
+  end
+
   test "should get index" do
     get :index
     assert_response :success
+    assert_not_nil assigns(:disasters)
   end
 
   test "should get new" do
@@ -11,24 +16,34 @@ class DisastersControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get create" do
-    get :create
+  test "should create disaster" do
+    assert_difference('Disaster.count') do
+      post :create, disaster: { description: @disaster.description, name: @disaster.name }
+    end
+
+    assert_redirected_to disaster_path(assigns(:disaster))
+  end
+
+  test "should show disaster" do
+    get :show, id: @disaster
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit
+    get :edit, id: @disaster
     assert_response :success
   end
 
-  test "should get update" do
-    get :update
-    assert_response :success
+  test "should update disaster" do
+    patch :update, id: @disaster, disaster: { description: @disaster.description, name: @disaster.name }
+    assert_redirected_to disaster_path(assigns(:disaster))
   end
 
-  test "should get delete" do
-    get :delete
-    assert_response :success
-  end
+  test "should destroy disaster" do
+    assert_difference('Disaster.count', -1) do
+      delete :destroy, id: @disaster
+    end
 
+    assert_redirected_to disasters_path
+  end
 end
