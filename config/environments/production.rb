fd5631b2d3bad003c1d7b6783c00717f22c6cc24
Yusuke Hosoi <yusuke0.hosoi@gmail.com>
2014-12-14 23:35:01 +0900

    メール設定の変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :enable_starttls_auto => true,
     :address => 'smtp.gmail.com',
-    :port => '587',
+    :port => 587,
     :domain => 'smtp.gmail.com',
     :authentication => 'plain',
     :user_name => 'take.five.mikaka@gmail.com',
