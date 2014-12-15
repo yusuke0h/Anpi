d100ff408dcf05ea50925bb0aea098a2cf83325e
Yusuke Hosoi <yusuke0.hosoi@gmail.com>
2014-12-15 13:30:50 +0900

    ユーザアップデートの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,20 @@ class User < ActiveRecord::Base
   has_many :confirmations
 
   def self.outside_update
+    OutsideUser.all. each do |ouser|
+      if User.where(id: ouser.id).first
+        user = User.find ouser.id
+        user.update(name: ouser.name, email: ouser.email, tel: ouser.tel)
+      else
+        User.create(name: ouser.name, email: ouser.email, tel: ouser.tel)
+      end
+    end
+
+    User.all.each do |user|
+      unless OutsideUser.where(id: user.id).first
+        user.destroy
+      end
+    end
+
   end
 end
