79c972d1bcedc387e685be028ae5031a2c260d1c
Yusuke Hosoi <yusuke0.hosoi@gmail.com>
2014-12-15 10:01:24 +0900

    外部DB経由だと関連付けができないので戻す

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,3 @@
 class User < ActiveRecord::Base
-  config = YAML.load_file('config/outside_database.yml')
-  establish_connection(config["db"])
-
   has_many :confirmations
-  self.primary_key = "id"
 end
