7f44ce758fded6269c58a7160a8e9aae32a3512e
Yusuke Hosoi <yusuke0.hosoi@gmail.com>
2014-12-15 09:55:35 +0900

    外部DB userをUserモデルにする

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
+  config = YAML.load_file('config/outside_database.yml')
+  establish_connection(config["db"])
+
   has_many :confirmations
+  self.primary_key = "id"
 end
